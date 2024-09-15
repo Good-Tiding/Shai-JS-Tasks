36b8b3e9ef133ac4d2ac813ef63b79914ddc348c
Good-Tiding <yorka22.business@gmail.com>
2024-09-15 17:01:48 +0300

    Add files via upload

---
+++ JS BOM/js/main.js
@@ -0,0 +1,18 @@
+//document.getElementById('root').innerHTML=screen.width; 
+//document.getElementById('root').innerHTML=screen.height; 
+//document.getElementById('root').innerHTML=window.location.href; 
+//document.getElementById('root').innerHTML=window.location.hostname; 
+//document.getElementById('root').innerHTML=window.location.pathname; 
+//document.getElementById('root').innerHTML=window.location.protocol; 
+
+function goBack()
+{
+  window.history.back();
+  
+}
+
+function goforward()
+{
+  window.history.forward();
+  
+}
